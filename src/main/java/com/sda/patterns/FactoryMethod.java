6387a23fa59fc532d3a71f4b755796df6cbdabec
Sonia Borysiak <sonia.borysiak@gmail.com>
2019-11-10 11:17:59 +0100

    Singleton package

--- src/main/java/com/sda/patterns/FactoryMethod.java
+++ src/main/java/com/sda/patterns/FactoryMethod.java
@@ -3,9 +3,9 @@ package com.sda.patterns;
 import java.awt.*;
 
 public class FactoryMethod {
-    public Robot makeRobot (String robotType){
+   /* public Robot makeRobot (String robotType){
         if (robotType=="ACCOUNTING ROBOT"){
 
         }
-    }
+    }*/
 }
